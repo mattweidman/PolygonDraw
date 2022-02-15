7f9be83e327fcf96f7d2986128e775c3340903ac
Matthew Weidman <weidman.matthew@gmail.com>
2022-02-14 21:50:35 -0800

    more ContainsPoint test cases

--- test/PolygonTests.cs
+++ test/PolygonTests.cs
@@ -479,6 +479,14 @@ namespace PolygonDrawTests
             Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(-1, 0)));
             // To the left aligned with top edge
             Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(-1, 2)));
+            // Above
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(0, 3)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(1, 3)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(2, 3)));
+            // Below
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(0, -1)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(1, -1)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(2, -1)));
 
             // Edges
             Assert.AreEqual(ContainmentType.BOUNDARY, polygon.ContainsPoint(new Vector2(1, 0)));
@@ -511,10 +519,21 @@ namespace PolygonDrawTests
             Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(0.5f, 1)));
             Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(1, 0)));
             Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(1.5f, 0)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(-5, 2)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(-5, 3)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(-5, 4)));
             // Inside
             Assert.AreEqual(ContainmentType.INSIDE, polygon.ContainsPoint(new Vector2(2, 2)));
             Assert.AreEqual(ContainmentType.INSIDE, polygon.ContainsPoint(new Vector2(2, 1)));
             Assert.AreEqual(ContainmentType.INSIDE, polygon.ContainsPoint(new Vector2(1, 2)));
+            Assert.AreEqual(ContainmentType.INSIDE, polygon.ContainsPoint(new Vector2(3, 2)));
+            Assert.AreEqual(ContainmentType.INSIDE, polygon.ContainsPoint(new Vector2(3.5f, 2)));
+            Assert.AreEqual(ContainmentType.INSIDE, polygon.ContainsPoint(new Vector2(2.5f, 1)));
+            // Above/below
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(2, 6)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(2, -5)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(1, 6)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(1, -5)));
 
             // Edges
             Assert.AreEqual(ContainmentType.BOUNDARY, polygon.ContainsPoint(new Vector2(1, 3)));
@@ -527,6 +546,58 @@ namespace PolygonDrawTests
             Assert.AreEqual(ContainmentType.BOUNDARY, polygon.ContainsPoint(new Vector2(0, 2)));
             Assert.AreEqual(ContainmentType.BOUNDARY, polygon.ContainsPoint(new Vector2(2, 4)));
             Assert.AreEqual(ContainmentType.BOUNDARY, polygon.ContainsPoint(new Vector2(4, 2)));
+        }
+
+        [Test]
+        public void ContainsPoint_Spikes()
+        {
+            Polygon polygon = new Polygon(new List<Vector2>()
+            {
+                new Vector2(0, 1),
+                new Vector2(1, 2),
+                new Vector2(2, 1),
+                new Vector2(3, 2),
+                new Vector2(4, 1),
+                new Vector2(5, 1),
+                new Vector2(6, 2),
+
+                new Vector2(6, 1),
+                new Vector2(5, 0),
+                new Vector2(4, 0),
+                new Vector2(3, 1),
+                new Vector2(2, 0),
+                new Vector2(1, 1),
+                new Vector2(0, 0),
+            });
+
+            // Height 1.5
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(0, 1.5f)));
+            Assert.AreEqual(ContainmentType.INSIDE, polygon.ContainsPoint(new Vector2(1, 1.5f)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(2, 1.5f)));
+            Assert.AreEqual(ContainmentType.INSIDE, polygon.ContainsPoint(new Vector2(3, 1.5f)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(4, 1.5f)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(5, 1.5f)));
+            Assert.AreEqual(ContainmentType.BOUNDARY, polygon.ContainsPoint(new Vector2(6, 1.5f)));
+
+            // Height 1
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(-0.5f, 1)));
+            Assert.AreEqual(ContainmentType.INSIDE, polygon.ContainsPoint(new Vector2(0.5f, 1)));
+            Assert.AreEqual(ContainmentType.INSIDE, polygon.ContainsPoint(new Vector2(1.5f, 1)));
+            Assert.AreEqual(ContainmentType.INSIDE, polygon.ContainsPoint(new Vector2(2.5f, 1)));
+            Assert.AreEqual(ContainmentType.INSIDE, polygon.ContainsPoint(new Vector2(3.5f, 1)));
+            Assert.AreEqual(ContainmentType.BOUNDARY, polygon.ContainsPoint(new Vector2(4.5f, 1)));
+            Assert.AreEqual(ContainmentType.INSIDE, polygon.ContainsPoint(new Vector2(5.5f, 1)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(6.5f, 1)));
+
+            // Height 0
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(-0.5f, 0)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(0.5f, 0)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(1.5f, 0)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(2.5f, 0)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(3.5f, 0)));
+            Assert.AreEqual(ContainmentType.BOUNDARY, polygon.ContainsPoint(new Vector2(4.5f, 0)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(5.5f, 0)));
+            Assert.AreEqual(ContainmentType.OUTSIDE, polygon.ContainsPoint(new Vector2(6.5f, 0)));
         }
     }
 }
